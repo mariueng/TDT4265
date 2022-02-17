83d19107a4ba78194dc923afe7b71d21dcfc4dd3
Marius Engen <marius@edyou.no>
2022-02-17 20:21:29 +0100

    Ass 2 finished

--- assignment2/task3.py
+++ assignment2/task3.py
@@ -60,30 +60,32 @@ if __name__ == "__main__":
     #     model_a, learning_rate, batch_size, shuffle_data,
     #     X_train, Y_train, X_val, Y_val,
     # )
+    # print('Training model a ...')
     # train_history_a, val_history_a = trainer_a.train(
     #     num_epochs)
+    # print('Done!')
 
     # plt.subplot(1, 2, 1)
     # utils.plot_loss(train_history["loss"],
     #                 "Base model train", npoints_to_average=10)
-    # utils.plot_loss(val_history["loss"],
-    #                 "Base model val", npoints_to_average=10)
     # utils.plot_loss(
     #     train_history_a["loss"], "Model a train", npoints_to_average=10)
+    # utils.plot_loss(val_history["loss"],
+    #                 "Base model validation")
     # utils.plot_loss(
-    #     val_history_a["loss"], "Model a val", npoints_to_average=10)
-    # plt.ylabel("Averaged cross entropy loss")
+    #     val_history_a["loss"], "Model a validation")
+    # plt.ylabel("Cross entropy loss")
     # plt.xlabel("Number of steps")
     # plt.legend()
     # plt.ylim([0, .4])
     # plt.subplot(1, 2, 2)
     # plt.ylim([0.90, 1.00])
     # utils.plot_loss(train_history["accuracy"], "Base model train")
-    # utils.plot_loss(val_history["accuracy"], "Base model val")
     # utils.plot_loss(
     #     train_history_a["accuracy"], "Model a train")
+    # utils.plot_loss(val_history["accuracy"], "Base model validation")
     # utils.plot_loss(
-    #     val_history_a["accuracy"], "Model a val")
+    #     val_history_a["accuracy"], "Model a validation")
     # plt.ylabel("Accuracy")
     # plt.xlabel("Number of steps")
     # plt.legend()
@@ -101,30 +103,32 @@ if __name__ == "__main__":
     #     model_b, learning_rate, batch_size, shuffle_data,
     #     X_train, Y_train, X_val, Y_val,
     # )
+    # print('Training model b ...')
     # train_history_b, val_history_b = trainer_b.train(
     #     num_epochs)
+    # print('Done!')
 
     # plt.subplot(1, 2, 1)
     # utils.plot_loss(train_history_a["loss"],
     #                 "Model a train", npoints_to_average=10)
-    # utils.plot_loss(val_history_a["loss"],
-    #                 "Model a val", npoints_to_average=10)
     # utils.plot_loss(
     #     train_history_b["loss"], "Model b train", npoints_to_average=10)
+    # utils.plot_loss(val_history_a["loss"],
+    #                 "Model a validation")
     # utils.plot_loss(
-    #     val_history_b["loss"], "Model b val", npoints_to_average=10)
-    # plt.ylabel("Averaged cross entropy loss")
+    #     val_history_b["loss"], "Model b validation")
+    # plt.ylabel("Cross entropy loss")
     # plt.xlabel("Number of steps")
     # plt.legend()
     # plt.ylim([0, .4])
     # plt.subplot(1, 2, 2)
     # plt.ylim([0.90, 1.00])
     # utils.plot_loss(train_history_a["accuracy"], "Model a train")
-    # utils.plot_loss(val_history_a["accuracy"], "Model a val")
     # utils.plot_loss(
     #     train_history_b["accuracy"], "Model b train")
+    # utils.plot_loss(val_history_a["accuracy"], "Model a validation")
     # utils.plot_loss(
-    #     val_history_b["accuracy"], "Model b val")
+    #     val_history_b["accuracy"], "Model b validation")
     # plt.ylabel("Accuracy")
     # plt.xlabel("Number of steps")
     # plt.legend()
@@ -145,30 +149,32 @@ if __name__ == "__main__":
     #     model_c, learning_rate, batch_size, shuffle_data,
     #     X_train, Y_train, X_val, Y_val,
     # )
+    # print('Training model c ...')
     # train_history_c, val_history_c = trainer_c.train(
     #     num_epochs)
+    # print('Done!')
 
     # plt.subplot(1, 2, 1)
     # utils.plot_loss(train_history_b["loss"],
     #                 "Model b train", npoints_to_average=10)
-    # utils.plot_loss(val_history_b["loss"],
-    #                 "Model b val", npoints_to_average=10)
     # utils.plot_loss(
     #     train_history_c["loss"], "Model c train", npoints_to_average=10)
+    # utils.plot_loss(val_history_b["loss"],
+    #                 "Model b validation")
     # utils.plot_loss(
-    #     val_history_c["loss"], "Model c val", npoints_to_average=10)
-    # plt.ylabel("Averaged cross entropy loss")
+    #     val_history_c["loss"], "Model c validation")
+    # plt.ylabel("Cross entropy loss")
     # plt.xlabel("Number of steps")
     # plt.legend()
     # plt.ylim([0, .4])
     # plt.subplot(1, 2, 2)
     # plt.ylim([0.90, 1.00])
     # utils.plot_loss(train_history_b["accuracy"], "Model b train")
-    # utils.plot_loss(val_history_b["accuracy"], "Model b val")
     # utils.plot_loss(
     #     train_history_c["accuracy"], "Model c train")
+    # utils.plot_loss(val_history_b["accuracy"], "Model b validation")
     # utils.plot_loss(
-    #     val_history_c["accuracy"], "Model c val")
+    #     val_history_c["accuracy"], "Model c validation")
     # plt.ylabel("Accuracy")
     # plt.xlabel("Number of steps")
     # plt.legend()
@@ -176,71 +182,161 @@ if __name__ == "__main__":
 
     # Task 4a and 4b
     # Keeping all settings unchanged from Task 3
+    # learning_rate = 0.02
+
+    # use_improved_sigmoid = True
+    # use_improved_weight_init = True
+    # use_momentum = True
+
+    # # 32-hidden layer unit network
+    # neurons_per_layer = [32, 10]
+    # model_32 = SoftmaxModel(
+    #     neurons_per_layer,
+    #     use_improved_sigmoid,
+    #     use_improved_weight_init)
+    # trainer_32 = SoftmaxTrainer(
+    #     momentum_gamma, use_momentum,
+    #     model_32, learning_rate, batch_size, shuffle_data,
+    #     X_train, Y_train, X_val, Y_val,
+    # )
+    # print("Training network with 32 hidden units...")
+    # train_history_32, val_history_32 = trainer_32.train(num_epochs)
+    # print("Done!")
+
+    # # 64-hidden layer unit network
+    # neurons_per_layer = [64, 10]
+    # model_64 = SoftmaxModel(
+    #     neurons_per_layer,
+    #     use_improved_sigmoid,
+    #     use_improved_weight_init)
+    # trainer_64 = SoftmaxTrainer(
+    #     momentum_gamma, use_momentum,
+    #     model_64, learning_rate, batch_size, shuffle_data,
+    #     X_train, Y_train, X_val, Y_val,
+    # )
+    # print("Training network with 64 hidden units...")
+    # train_history_64, val_history_64 = trainer_64.train(num_epochs)
+    # print("Done!")
+
+    # # 128-hidden layer unit network
+    # neurons_per_layer = [128, 10]
+    # model_128 = SoftmaxModel(
+    #     neurons_per_layer,
+    #     use_improved_sigmoid,
+    #     use_improved_weight_init)
+    # trainer_128 = SoftmaxTrainer(
+    #     momentum_gamma, use_momentum,
+    #     model_128, learning_rate, batch_size, shuffle_data,
+    #     X_train, Y_train, X_val, Y_val,
+    # )
+    # print("Training network with 128 hidden units...")
+    # train_history_128, val_history_128 = trainer_128.train(num_epochs)
+    # print("Done!")
+
+    # # Plot comparisons
+
+    # # Cross entropy loss
+    # plt.figure(figsize=(20, 12))
+    # utils.plot_loss(val_history_32["loss"], "Model 32")
+    # utils.plot_loss(val_history_64["loss"], "Model 64")
+    # utils.plot_loss(val_history_128["loss"], "Model 128")
+    # plt.ylabel("Validation cross entropy loss")
+    # plt.xlabel("Number of steps")
+    # plt.legend()
+    # plt.ylim([0, .4])
+    # plt.show()
+
+    # # Accuracy
+    # plt.figure(figsize=(20, 12))
+    # plt.ylim([0.90, 1.00])
+    # utils.plot_loss(val_history_32["accuracy"], "Model 32")
+    # utils.plot_loss(val_history_64["accuracy"], "Model 64")
+    # utils.plot_loss(val_history_128["accuracy"], "Model 128")
+    # plt.ylabel("Validation accuracy")
+    # plt.xlabel("Number of steps")
+    # plt.legend()
+    # plt.show()
+
+    # Task 4d)
+
+    # Keep settings from earlier.
+    # learning_rate = 0.02
+
+    # use_improved_sigmoid = True
+    # use_improved_weight_init = True
+    # use_momentum = True
+
+    # neurons_per_layer = [60, 60, 10]
+    # model_2_hidden_layers = SoftmaxModel(
+    #     neurons_per_layer,
+    #     use_improved_sigmoid,
+    #     use_improved_weight_init)
+    # trainer_2_hidden_layers = SoftmaxTrainer(
+    #     momentum_gamma, use_momentum,
+    #     model_2_hidden_layers, learning_rate, batch_size, shuffle_data,
+    #     X_train, Y_train, X_val, Y_val,
+    # )
+    # print("Training network with 2 hidden layers...")
+    # train_history_2_hidden_layers, val_history_2_hidden_layers = trainer_2_hidden_layers.train(num_epochs)
+    # print("Done!")
+
+    # # Loss
+    # plt.figure(figsize=(20, 12))
+    # utils.plot_loss(train_history_2_hidden_layers["loss"], "Training", npoints_to_average=10)
+    # utils.plot_loss(val_history_2_hidden_layers["loss"], "Validation")
+    # plt.ylabel("Cross entropy loss")
+    # plt.xlabel("Number of steps")
+    # plt.legend()
+    # plt.ylim([0, 1])
+    # plt.show()
+
+    # # Accuracy
+    # plt.figure(figsize=(20, 12))
+    # plt.ylim([0.90, 1.00])
+    # utils.plot_loss(train_history_2_hidden_layers["accuracy"], "Training")
+    # utils.plot_loss(val_history_2_hidden_layers["accuracy"], "Validation")
+    # plt.ylabel("Accuracy")
+    # plt.xlabel("Number of steps")
+    # plt.legend()
+    # plt.show()
+
+    # Task 4e
     learning_rate = 0.02
 
     use_improved_sigmoid = True
     use_improved_weight_init = True
     use_momentum = True
 
-    # 32-hidden layer unit network
-    neurons_per_layer = [32, 10]
-    model_32 = SoftmaxModel(
+    neurons_per_layer = [64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 10]
+    model_10_hidden_layers = SoftmaxModel(
         neurons_per_layer,
         use_improved_sigmoid,
         use_improved_weight_init)
-    trainer_32 = SoftmaxTrainer(
+    trainer_10_hidden_layers = SoftmaxTrainer(
         momentum_gamma, use_momentum,
-        model_32, learning_rate, batch_size, shuffle_data,
+        model_10_hidden_layers, learning_rate, batch_size, shuffle_data,
         X_train, Y_train, X_val, Y_val,
     )
-    train_history_32, val_history_32 = trainer_32.train(num_epochs)
+    print("Training network with 10 hidden layers...")
+    train_history_10_hidden_layers, val_history_10_hidden_layers = trainer_10_hidden_layers.train(num_epochs)
+    print("Done!")
 
-    # 64-hidden layer unit network
-    neurons_per_layer = [64, 10]
-    model_64 = SoftmaxModel(
-        neurons_per_layer,
-        use_improved_sigmoid,
-        use_improved_weight_init)
-    trainer_64 = SoftmaxTrainer(
-        momentum_gamma, use_momentum,
-        model_64, learning_rate, batch_size, shuffle_data,
-        X_train, Y_train, X_val, Y_val,
-    )
-    train_history_64, val_history_64 = trainer_64.train(num_epochs)
-
-    # 128-hidden layer unit network
-    neurons_per_layer = [128, 10]
-    model_128 = SoftmaxModel(
-        neurons_per_layer,
-        use_improved_sigmoid,
-        use_improved_weight_init)
-    trainer_128 = SoftmaxTrainer(
-        momentum_gamma, use_momentum,
-        model_128, learning_rate, batch_size, shuffle_data,
-        X_train, Y_train, X_val, Y_val,
-    )
-    train_history_128, val_history_128 = trainer_128.train(num_epochs)
-
-    # Plot comparisons
-
-    # Cross entropy loss
+    # Loss
     plt.figure(figsize=(20, 12))
-    utils.plot_loss(val_history_32["loss"], "Model 32")
-    utils.plot_loss(val_history_64["loss"], "Model 64")
-    utils.plot_loss(val_history_128["loss"], "Model 128")
-    plt.ylabel("Validation cross entropy loss")
+    utils.plot_loss(train_history_10_hidden_layers["loss"], "Training", npoints_to_average=10)
+    utils.plot_loss(val_history_10_hidden_layers["loss"], "Validation")
+    plt.ylabel("Cross entropy loss")
     plt.xlabel("Number of steps")
     plt.legend()
-    plt.ylim([0, .4])
+    plt.ylim([0, 1])
     plt.show()
 
     # Accuracy
     plt.figure(figsize=(20, 12))
     plt.ylim([0.90, 1.00])
-    utils.plot_loss(val_history_32["accuracy"], "Model 32")
-    utils.plot_loss(val_history_64["accuracy"], "Model 64")
-    utils.plot_loss(val_history_128["accuracy"], "Model 128")
-    plt.ylabel("Validation accuracy")
+    utils.plot_loss(train_history_10_hidden_layers["accuracy"], "Training")
+    utils.plot_loss(val_history_10_hidden_layers["accuracy"], "Validation")
+    plt.ylabel("Accuracy")
     plt.xlabel("Number of steps")
     plt.legend()
     plt.show()
