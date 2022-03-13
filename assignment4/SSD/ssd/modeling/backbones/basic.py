52cea4a6e8e11c19882d550de74bbfb83140ef69
Marius Engen <mariueng@oppdal.idi.ntnu.no>
2022-03-13 15:39:04 +0100

    Impl. basic model

--- assignment4/SSD/ssd/modeling/backbones/basic.py
+++ assignment4/SSD/ssd/modeling/backbones/basic.py
@@ -20,6 +20,73 @@ class BasicModel(torch.nn.Module):
         super().__init__()
         self.out_channels = output_channels
         self.output_feature_shape = output_feature_sizes
+        num_filters = 32
+        self.feature_map_zero = torch.nn.Sequential(
+            torch.nn.Conv2d(
+                in_channels=image_channels,
+                out_channels=num_filters,
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ),
+            torch.nn.MaxPool2d(
+                kernel_size=2,
+                stride=2
+            ),
+            torch.nn.ReLU(),
+
+            torch.nn.Conv2d(num_filters, num_filters * 2, 3, 1, 1),
+            torch.nn.MaxPool2d(2,2),
+            torch.nn.ReLU(),
+            
+            torch.nn.Conv2d(num_filters * 2, num_filters * 2, 3, 1, 1),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(num_filters * 2, self.out_channels[0], 3, 2, 1)
+        )
+
+        self.feature_map_one = torch.nn.Sequential(
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(self.out_channels[0], 128, 3, 1, 1),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(128, self.out_channels[1], 3, 2, 1),
+        )
+
+        self.feature_map_two = torch.nn.Sequential(
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(self.out_channels[1], 256, 3, 1, 1),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(256, self.out_channels[2], 3, 2, 1),
+        )
+
+        self.feature_map_three = torch.nn.Sequential(
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(self.out_channels[2], 128, 3, 1, 1),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(128, self.out_channels[3], 3, 2, 1),
+        )
+
+        self.feature_map_four = torch.nn.Sequential(
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(self.out_channels[3], 128, 3, 1, 1),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(128, self.out_channels[4], 3, 2, 1),
+        )
+
+        self.feature_map_five = torch.nn.Sequential(
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(self.out_channels[4], 128, 3, 1, 1),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(128, self.out_channels[5], 3, 1, 0)
+        )
+        
+        self.feature_maps = [
+            self.feature_map_zero,
+            self.feature_map_one,
+            self.feature_map_two,
+            self.feature_map_three,
+            self.feature_map_four,
+            self.feature_map_five,
+        ]
 
     def forward(self, x):
         """
@@ -35,6 +102,11 @@ class BasicModel(torch.nn.Module):
             shape(-1, output_channels[0], 38, 38),
         """
         out_features = []
+
+        out_features.append(self.feature_map_zero(x))
+        for i in range(1, len(self.feature_maps)):
+            out_features.append(self.feature_maps[i](out_features[i - 1]))
+
         for idx, feature in enumerate(out_features):
             out_channel = self.out_channels[idx]
             h, w = self.output_feature_shape[idx]
